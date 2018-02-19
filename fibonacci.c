2d91cb650c7d29fef424080201a1ac1b57611e92
sankarsanthiya <31505543+sankarsanthiya@users.noreply.github.com>
2018-02-19 16:33:03 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,4 +1,4 @@
-#include,stdio.h>
+#include<stdio.h>
 int main()
 {
 int t,t1=0,t2=1,n,i;
@@ -6,9 +6,9 @@ printf("enter the number");
 scanf("%d",&n);
 for(i=0;i<=n;i++)
 {
+  printf("%d\t",t1);
 t=t1+t2;
 t1=t2;
 t2=t;
-printf("%d",t);
 }
 }
