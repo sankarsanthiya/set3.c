2f332e1f3aa2ddc2403a5fa58dcd4cfa0d94fd8c
sankarsanthiya <31505543+sankarsanthiya@users.noreply.github.com>
2018-02-19 16:53:01 +0530

    Create spaces.c

---
+++ spaces.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+   char a[100];
+   int b,count=0,i;
+   scanf("%s",a);
+   b=strlen(a);
+   for(i=0;i<b;i++)
+   {
+       if(s[i]==' ')
+       count++;
+       printf("%d",count);
+       else
+       printf("%d",count);
+   }
+
+    return 0;
+}
