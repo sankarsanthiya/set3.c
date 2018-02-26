d616740e613490a994fc9fb56b7f3972e1913d97
sankarsanthiya <31505543+sankarsanthiya@users.noreply.github.com>
2018-02-26 16:24:45 +0530

    Update swap3.c

--- swap3.c
+++ swap3.c
@@ -3,9 +3,9 @@ int main()
 {
 int a,b,t;
 printf("enter the two number");
-scanf("%d%d",&a,&b);
-a=t;
-b=a;
-t=a;
+scanf("%d %d",&a,&b);
+  a=a+b;
+  b=a-b;
+  a=a-b;
 printf("after swaping:%d %d",a,b);
 }
